e1f812277560dd503b4c802f23c4138c69bc1867
Nate Nault <nate@nathannault.com>
2017-05-19 14:44:14 +0800

    Initial commit

---
+++ solutions/_tpl/spec.js
@@ -0,0 +1,7 @@
+// Test Suite
+describe('Mocha', function () {
+  // Test spec (unit test)
+  it('should test my function', function () {
+    expect(true).to.be.ok;
+  });
+});
---
+++ solutions/_tpl/src.js
@@ -0,0 +1,3 @@
+var exampleFunction = function() {
+  return true;
+};
